d82cbf6e0234601bbeb15fb39b17164e79ac171e
Mahlet <mahletdemissie6@gmail.com>
2023-11-20 16:54:33 +0300

    Butajira reports continued

--- openmrs/apps/reports/sql/new/OR.sql
+++ openmrs/apps/reports/sql/new/OR.sql
@@ -1,12 +1,12 @@
     select
 
-        concat(answer2.name ," surgeries:",answer.name) as "Catagory",
+        concat(answer2.name, " surgeries: ", answer.name) as "Category",
 
         count(o.obs_id and o2.obs_id) as "Patient Count"
 
     from concept_answer ca
 
-        join concept_name question on question.concept_id = ca.concept_id and question.concept_name_type = 'FULLY_SPECIFIED' AND question.name = 'ON Specialty of surgery'
+        join concept_name question on question.concept_id = ca.concept_id and question.concept_name_type = 'FULLY_SPECIFIED' AND question.name = 'Department, operative notes'
 
         join concept_name answer on answer.concept_id = ca.answer_concept and answer.concept_name_type = 'SHORT'
 
@@ -20,15 +20,13 @@
 
         LEFT JOIN obs o2 on o.encounter_id = o2.encounter_id and o2.value_coded = answer2.concept_id and o2.voided=0 and CAST(o2.date_created AS DATE) BETWEEN '#startDate#' AND '#endDate#'
 
-     
-
     GROUP BY answer2.name, answer.name
 
 UNION
 
     select
 
-        concat("Total tally for ", answer.name ," surgeries:") as "Catagory",
+        concat("Total tally for ", answer.name ," surgeries:") as "Category",
 
         count(o.obs_id) as "Patient Count"
 
@@ -46,7 +44,7 @@ UNION
 
     select
 
-        concat(sa.status, " Surgeries") as "Catagory",
+        concat(sa.status, " Surgeries") as "Category",
 
         count(sa2.surgical_appointment_id) as "Patient Count"
 
