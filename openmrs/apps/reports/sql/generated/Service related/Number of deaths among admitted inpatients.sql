081d568309b8e73b1b338a2ee8f4e817385e06bc
light <ahmihassu@gmail.com>
2024-03-13 08:01:17 +0300

    Butajira report

--- openmrs/apps/reports/sql/generated/Service related/Number of deaths among admitted inpatients.sql
+++ openmrs/apps/reports/sql/generated/Service related/Number of deaths among admitted inpatients.sql
@@ -1,8 +1,17 @@
 SELECT 
-COUNT(DISTINCT person.person_id) AS "Number of deaths among admitted inpatients"
+loc.name as "Ipd Location",
+SUM(IF(form_under_obs0.encounter_id IS NOT NULL,1,0)) AS "Number of deaths among admitted inpatients",
+COUNT(DISTINCT bpam.patient_id) AS "Total Admitted",
+COUNT(DISTINCT CASE WHEN bpam.date_stopped IS NOT NULL THEN bpam.patient_id END) AS "Total Discharged"
 FROM person
 JOIN encounter on person.person_id = encounter.patient_id AND date(encounter.encounter_datetime) between '#startDate#' and '#endDate#' 
-JOIN obs form_under_obs0 ON encounter.encounter_id = form_under_obs0.encounter_id
+LEFT JOIN obs form_under_obs0 ON encounter.encounter_id = form_under_obs0.encounter_id
                         AND form_under_obs0.concept_id IN (SELECT concept_id from concept_name WHERE concept_name_type = "FULLY_SPECIFIED" AND name LIKE "Death notification")
 JOIN visit ON encounter.visit_id = visit.visit_id
 JOIN visit_type ON visit.visit_type_id = visit_type.visit_type_id AND visit_type.name = "IPD"
+JOIN encounter en ON en.visit_id = encounter.visit_id
+JOIN bed_patient_assignment_map bpam ON bpam.encounter_id = en.encounter_id
+JOIN bed_location_map blm ON bpam.bed_id = blm.bed_id
+JOIN location par ON blm.location_id = par.location_id
+JOIN location loc ON loc.location_id = par.parent_location
+GROUP BY loc.name
--- openmrs/apps/reports/sql/generated/Surgery related/Number of operated inpatient with new surgical site infection arising before discharge.sql
+++ openmrs/apps/reports/sql/generated/Surgery related/Number of operated inpatient with new surgical site infection arising before discharge.sql
@@ -1,5 +1,6 @@
 SELECT 
-COUNT(DISTINCT person.person_id) AS "Number of operated inpatient with new surgical site infection arising before discharge"
+COUNT(DISTINCT person.person_id) AS "Number of operated inpatient with new surgical site infection arising before discharge",
+(SELECT name FROM concept_name WHERE concept_id = form_under_obs2.value_coded AND concept_name_type = "FULLY_SPECIFIED") AS "Department"
 FROM person
 JOIN encounter on person.person_id = encounter.patient_id AND date(encounter.encounter_datetime) between '#startDate#' and '#endDate#' 
 JOIN visit ON encounter.visit_id = visit.visit_id
@@ -9,4 +10,7 @@ LEFT JOIN obs form_under_obs0 ON encounter.encounter_id = form_under_obs0.encoun
                         AND form_under_obs0.concept_id IN (SELECT concept_id from concept_name WHERE concept_name_type = "FULLY_SPECIFIED" AND name LIKE  "Did the patient develop a new surgical site infection?")
 LEFT JOIN obs form_under_obs1 ON encounter.encounter_id = form_under_obs1.encounter_id
                         AND form_under_obs1.concept_id IN (SELECT concept_id from concept_name WHERE concept_name_type = "FULLY_SPECIFIED" AND name LIKE "Operative Notes")
+LEFT JOIN obs form_under_obs2 ON encounter.encounter_id = form_under_obs2.encounter_id
+                        AND form_under_obs2.concept_id IN (SELECT concept_id from concept_name WHERE concept_name_type = "FULLY_SPECIFIED" AND name LIKE "Department, operative notes")
 WHERE (form_under_obs1.obs_id IS NOT NULL AND form_under_obs0.obs_id IS NOT NULL)
+GROUP BY form_under_obs2.value_coded
